e978e9ee3d344aff331a06954cf84fda1e83a20e
acousland <acousland@gmail.com>
2015-05-12 09:43:35 +0800

    Updated with transparent polygons

--- main.R
+++ main.R
@@ -16,28 +16,22 @@ source ('Chebyschev_Function.R')
 local.connection <- odbcConnect("RTV", believeNRows=FALSE)
 
 # Query the database and put the results into the data frame logging.results
-logger.results <- sqlQuery(local.connection,"SELECT * FROM ELSPEC.RMS_TRAINING where ts between '01/Feb/15 09:00:00 AM' and '28/Mar/15 09:00:00 AM';")
+logger.results <- sqlQuery(local.connection,"SELECT * FROM ELSPEC.RMS_TRAINING where ts between '17/Mar/15 08:00:00 AM' and '17/Mar/15 03:00:00 PM';")
 
 #Order by timestamp
-logger.results <- logger.results[with(logger.results, order(logger.results$TIMESTAMP)),]
-
-# Set filtered data subset times and subset the data
-StartTime <- as.POSIXct("2015-03-17 10:00:00", format = "%Y-%m-%d %H:%M:%OS", tz = "UTC")
-EndTime <- as.POSIXct("2015-03-17 11:00:00", format = "%Y-%m-%d %H:%M:%OS", tz = "UTC")
-
-filtered.results <- subset(logger.results, logger.results$TIMESTAMP >= StartTime & logger.results$TIMESTAMP <= EndTime)
+logger.results <- logger.results[with(logger.results, order(logger.results$TS)),]
 
 # Initialise probability of fault column and window length
-filtered.results$PrFault <- 0
+logger.results$PrFault <- 0
 window.length = 5000
 
 # Run Chebyschev analysis on results
-filtered.results <- Chebyschev(filtered.results,window.length)
+logger.results <- Chebyschev(logger.results,window.length)
 
 # Display Results
-par(mfcol=c(2,1))
-plot(filtered.results$TIMESTAMP,filtered.results$PrFault)
-plot(filtered.results$TIMESTAMP,filtered.results$RMSI1)
+plot(logger.results$TS,logger.results$RMSI1)
+polygon(logger.results$TS,logger.results$PrFault*max(logger.results$RMSI1), col =rgb(1,0,0,alpha=0.3),xlab="",ylab="",yaxt="n")
+axis(4)
 
 # Zoom and view
 StartTime2 <- as.POSIXct("2015-03-17 10:30:00", format = "%Y-%m-%d %H:%M:%OS", tz = "UTC")
@@ -47,3 +41,10 @@ filtered.results2 <- subset(filtered.results, filtered.results$TIMESTAMP >= Star
 par(mfcol=c(2,1))
 plot(filtered.results2$TIMESTAMP,filtered.results2$PrFault)
 plot(filtered.results2$TIMESTAMP,filtered.results2$RMSI1)
+
+
+
+
+
+plot(logger.results$TS,logger.results$RMSI1)
+polygon(logger.results$TS,logger.results$FAULT*max(logger.results$RMSI1), col = "firebrick1")
