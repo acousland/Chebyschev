f7a7379e87f32fd596efe9a0bff678919ef62acc
acousland <acousland@gmail.com>
2015-05-15 16:30:11 +1000

    Changed to superimpose load current

--- main.R
+++ main.R
@@ -22,34 +22,22 @@ odbcCloseAll()
 local.connection <- odbcConnect("RTV", believeNRows=FALSE)
 
 # Query the database and put the results into the data frame logging.results
-logger.results <- sqlQuery(local.connection,"SELECT * FROM ELSPEC.RMS_TRAINING where ts between '17/Mar/15 08:00:00 AM' and '17/Mar/15 03:00:00 PM';")
+logger.results <- sqlQuery(local.connection,"SELECT * FROM ELSPEC.RMS_TRAINING where ts between '17/Mar/15 08:00:00 AM' and '17/Mar/15 05:00:00 PM';")
 odbcCloseAll()
 
 #Order by timestamp and force local timestamp
 logger.results <- logger.results[with(logger.results, order(logger.results$TS)),]
-logger.results$TS <- force_tz(logger.results$TS,"AEST")
+logger.results$TS <- force_tz(logger.results$TS,"UTC")
 
 # Initialise probability of fault column and window length
 logger.results$PrFault <- 0
-window.length = 1000
+window.length = 1440
+
+# Superimpose load current on fault phase
+logger.results$RMSI1 <- logger.results$RMSI1 + logger.results$RMSI2 
 
 # Run Chebyschev analysis on results
 logger.results <- Chebyschev(logger.results,window.length)
-
-# Display Results
-plot(logger.results$TS,logger.results$RMSI1, type="l")
-polygon(logger.results$TS,logger.results$PrFault*max(logger.results$RMSI1), col =rgb(1,0,0,alpha=0.3),xlab="",ylab="",yaxt="n",border = NA)
-axis(4)
-
-# Zoom and view
-StartTime <- force_tz(as.POSIXct("2015-03-17 08:45:00 AM", format = "%Y-%m-%d %H:%M:%OS"),"AEST")
-EndTime <- force_tz(as.POSIXct("2015-03-17 11:00:00 AM", format = "%Y-%m-%d %H:%M:%OS"),"AEST")
-filtered.results <- subset(logger.results, logger.results$TS >= StartTime & logger.results$TS <= EndTime)
-
-# Display Results
-plot.I1 <- ggplot(filtered.results, aes(x=TS, y=RMSI1,colour=FAULT)) + geom_point()
-plot.PrFault <- ggplot(filtered.results, aes(x=TS, y=PrFault, ymin=0, ymax=PrFault)) + geom_ribbon()
-grid.arrange(plot.I1, plot.PrFault, ncol=1)
 
 # Optimise the trigger threshold
 results <- Threshold_Optimise(logger.results,0,1,0.01)
@@ -58,7 +46,7 @@ plot(results$Threshold,results$success)
 
 # Perform thresholding as per optimum value
 logger.results.threshold <- logger.results
-logger.results.threshold$PrFault2 <- ifelse(logger.results.threshold$PrFault<threshold,0,logger.results.threshold$PrFault)
+logger.results.threshold$PrFault2 <- ifelse(logger.results.threshold$PrFault<threshold,0,1)
 
 # Measure performance
 performance <- logger.results.threshold %>%
@@ -69,8 +57,8 @@ print(performance$Score)
 print(paste("Score =",performance$Score[2]-performance$Score[1],"/",sum(logger.results$FAULT==TRUE)))
 
 # Interrogate results
-StartTime <- force_tz(as.POSIXct("2015-03-17 08:45:00", format = "%Y-%m-%d %H:%M:%OS"),"AEST")
-EndTime <- force_tz(as.POSIXct("2015-03-17 11:00:00", format = "%Y-%m-%d %H:%M:%OS"),"AEST")
+StartTime <- force_tz(as.POSIXct("2015-03-17 13:18:00", format = "%Y-%m-%d %H:%M:%OS"),"UTC")
+EndTime <- force_tz(as.POSIXct("2015-03-17 13:19:00", format = "%Y-%m-%d %H:%M:%OS"),"UTC")
 logger.results.threshold2 <- subset(logger.results.threshold, logger.results.threshold$TS >= StartTime & logger.results.threshold$TS <= EndTime)
 plot(logger.results.threshold2$TS,logger.results.threshold2$RMSI1, type="l")
 polygon(logger.results.threshold2$TS,logger.results.threshold2$PrFault2*max(logger.results.threshold2$RMSI1), col =rgb(1,0,0,alpha=0.3),xlab="",ylab="",yaxt="n",border = NA)
